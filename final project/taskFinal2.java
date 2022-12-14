f0246b8a4c50c03956a13b93ee9888905631221a
PrusovAB <Djromeo93@mail.ru>
2022-12-14 17:00:27 +0300

    add

---
+++ final project/taskFinal2.java
@@ -0,0 +1,10 @@
+package final project;
+
+public class taskFinal2 {
+   public static void main(String[] args) {
+
+      Cat cat1 = new Cat(100, "Вася", "Дворняга", "Рыжий", true, 5, 5);
+
+
+  }
+}
---
+++ final project/taskFinal3.java
@@ -0,0 +1,11 @@
+package final project;
+
+public class taskFinal3 {
+   public static void main(String[] args) {
+
+      Cat cat1 = new Cat(100, "Вася", "Дворняга", "Рыжий", true, 5, 5);
+
+      System.out.println(cat1.toString(100));
+
+  }
+}
